a496e404c95777c482a069ec19683d7169844d0b
danielbirsan <75643437+danielbirsan@users.noreply.github.com>
2024-05-15 10:57:46 +0300

    Update erorrs.h

--- erori/erorrs.h
+++ erori/erorrs.h
@@ -6,18 +6,18 @@
 
 class PurchaseException : public std::runtime_error {
 public:
-    explicit PurchaseException(const std::string& message) : std::runtime_error("Purchase error" +message) {}
+    explicit PurchaseException(const std::string& message) : std::runtime_error(message) {}
 };
 
 
 class InvalidInputException : public std::invalid_argument {
 public:
-    explicit InvalidInputException(const std::string& message) : std::invalid_argument("Invalid Input:"+message) {}
+    explicit InvalidInputException(const std::string& message) : std::invalid_argument(message) {}
 };
 
 class InsufficientPointsException : public PurchaseException {
 public:
-    explicit InsufficientPointsException(const std::string& message) : PurchaseException("Insufficient Points:"+message) {}
+    explicit InsufficientPointsException(const std::string& message) : PurchaseException(message) {}
 };
 
 
